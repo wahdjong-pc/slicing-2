f4a37492ccd9c7f00423f089f2cbbd15422ac587
Wahyu Pratama <wahyupratamadev@gmail.com>
2024-07-29 21:41:28 +0700

    Add section testimonials and main.js for scrolling testimonials

---
+++ js/main.js
@@ -0,0 +1,30 @@
+const scrollContainer = document.querySelector(".testimonial__right");
+
+let isDown = false;
+let startX;
+let scrollLeft;
+
+scrollContainer.addEventListener("mousedown", (e) => {
+  isDown = true;
+  scrollContainer.classList.add("active");
+  startX = e.pageX - scrollContainer.offsetLeft;
+  scrollLeft = scrollContainer.scrollLeft;
+});
+
+scrollContainer.addEventListener("mouseleave", () => {
+  isDown = false;
+  scrollContainer.classList.remove("active");
+});
+
+scrollContainer.addEventListener("mouseup", () => {
+  isDown = false;
+  scrollContainer.classList.remove("active");
+});
+
+scrollContainer.addEventListener("mousemove", (e) => {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - scrollContainer.offsetLeft;
+  const walk = (x - startX) * 2; // Scroll-fast
+  scrollContainer.scrollLeft = scrollLeft - walk;
+});
